9a81aad2bdb3313f324ce1b9878bdae23ba7a467
Rinat Sungatullin <rinat.sungatullin.work@gmail.com>
2025-04-06 22:30:31 +0400

    добавлена поддержка скобок, добавлена валидация, добавлено логирование

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,4 @@
+import com.sun.jdi.InvalidTypeException;
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -6,7 +7,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Класс калькулятора
+ * Класс калькулятор
  */
 public class Calculator {
   private static final String POWER = "^";
@@ -21,19 +22,34 @@ public class Calculator {
   public void run() {
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("1 - вветси пример\n" +
-                       "2 - посмотреть историю\n" +
-                       "3 - очистить историю");
+    System.out.println("""
+            1 - ввести пример
+            2 - посмотреть историю
+            3 - очистить историю""");
     int userChoice = scan.nextInt();
 
     FileManager fileManager = new FileManager();
 
     switch (userChoice) {
       case 1: {
-        System.out.println("Введи пример (между операциями должен быть пробел)");
-        Scanner mathExampleInput = new Scanner(System.in);
-        String mathExample = mathExampleInput.nextLine();
-        calculate(mathExample);
+        System.out.println("Введи пример (между операциями должен быть пробел), например: (2 + 3) * 4 ^ 2");
+        Scanner scanner = new Scanner(System.in);
+
+        String inputMathExample = scanner.nextLine();
+
+        String mathExample = inputMathExample;
+
+        try {
+          verifyMathExample(inputMathExample);
+          if (mathExample.contains("(")) {
+            mathExample = (throwBrackets(mathExample));
+          }
+          mathExample = calculate(mathExample);
+          System.out.println(inputMathExample + " = " + mathExample);
+        } catch (Exception ex) {
+          fileManager.writeToFile("log.txt", ex.getMessage());
+          System.out.println(ex.getMessage());
+        }
         break;
       }
       case 2: {
@@ -52,66 +68,125 @@ public class Calculator {
   }
 
   /**
-   * Вычислить
+   * Проверить корректность примера
    *
    * @param mathExample пример
+   * @throws InvalidTypeException пример некорректный
    */
-  public void calculate(String mathExample) {
-    ArrayList<String> splitMathExample = new ArrayList<>(Arrays.asList(mathExample.split(" "))); //Коневертация строки с примером в ArrayList, разделение по пробелам
+  private void verifyMathExample(String mathExample) throws InvalidTypeException {
+    char[] correctSymbols = {'+', '-', '*', '/', '^', ' ', '.', '(', ')'};
 
-    try {
-      while (splitMathExample.stream().count() > 1) {
-        if (splitMathExample.contains(POWER)) {
-          splitMathExample = performOperation(splitMathExample, POWER);
-        }
-        else if (splitMathExample.contains(MULTIPLICATION) && !splitMathExample.contains(DIVIDE)) {
-          splitMathExample = performOperation(splitMathExample, MULTIPLICATION);
-        }
-        else if (splitMathExample.contains(DIVIDE) && !splitMathExample.contains(MULTIPLICATION)) {
-          splitMathExample = performOperation(splitMathExample, DIVIDE);
-        }
-        else if (splitMathExample.contains(MULTIPLICATION) && splitMathExample.contains(DIVIDE)) {
-          if (splitMathExample.indexOf(MULTIPLICATION) < splitMathExample.indexOf(DIVIDE)) {
-            splitMathExample = performOperation(splitMathExample, MULTIPLICATION);
-          }
-          else if (splitMathExample.indexOf(DIVIDE) < splitMathExample.indexOf(MULTIPLICATION)) {
-            splitMathExample = performOperation(splitMathExample, DIVIDE);
-          }
-        }
-        else if (splitMathExample.contains(PLUS) && !splitMathExample.contains(MINUS)) {
-          splitMathExample = performOperation(splitMathExample, PLUS);
-        }
-        else if (splitMathExample.contains(MINUS) && !splitMathExample.contains(PLUS)) {
-          splitMathExample = performOperation(splitMathExample, MINUS);
-        }
-        else if (splitMathExample.contains(PLUS) && splitMathExample.contains(MINUS)) {
-          if (splitMathExample.indexOf(PLUS) < splitMathExample.indexOf(MINUS)) {
-            splitMathExample = performOperation(splitMathExample, PLUS);
-          }
-          else if (splitMathExample.indexOf(MINUS) < splitMathExample.indexOf(PLUS)) {
-            splitMathExample = performOperation(splitMathExample, MINUS);
-          }
-        }
+    int openBracketCounter = 0;
+    int closeBracketCounter = 0;
+
+    boolean isValid = true;
+
+    for (int i = 0; i < mathExample.length(); i++) {
+      if (!Arrays.asList(correctSymbols).contains(mathExample.charAt(i)) && Character.isLetter(mathExample.charAt(i))) {
+        isValid = false;
+        break;
       }
+      if (mathExample.charAt(i) == '(') {
+        openBracketCounter++;
+      } else if (mathExample.charAt(i) == ')') {
+        closeBracketCounter++;
+      }
+    }
 
-      String mathExampleResult = mathExample + " = " + splitMathExample.get(0);
+    if (openBracketCounter != closeBracketCounter) {
+      isValid = false;
+    }
 
-      FileManager fileManager = new FileManager();
-      fileManager.writeToFile("history.txt", mathExampleResult);
-
-      System.out.println(mathExampleResult);
-    } catch (IllegalArgumentException e) {
-      System.out.println(e.getMessage());
+    if (!isValid) {
+      throw new InvalidTypeException("Incorrect math example");
     }
   }
 
   /**
+   * получить значения в скобках
+   *
+   * @param mathExample пример
+   * @return результат вычисления в скобках
+   */
+  private String throwBrackets(String mathExample) {
+    StringBuilder sb = new StringBuilder(mathExample);
+
+    int openBracketIndex = 0;
+    int closeBracketIndex = 0;
+
+    for (int i = 0; i < sb.length(); i++) {
+      if (sb.charAt(i) == '(') {
+        openBracketIndex = i;
+      }
+      else if (sb.charAt(i) == ')') {
+        closeBracketIndex = i;
+        String examplePart = sb.substring(openBracketIndex + 1, closeBracketIndex);
+        calculate(examplePart);
+        sb.replace(openBracketIndex, closeBracketIndex + 1, calculate(examplePart));
+        i = -1;
+      }
+    }
+    return sb.toString();
+  }
+
+  /**
+   * Вычислить пример без скобок
+   *
+   * @param mathExample пример
+   */
+  private String calculate(String mathExample) {
+    ArrayList<String> splitMathExample = new ArrayList<>(Arrays.asList(mathExample.split(" "))); //Коневертация строки с примером в ArrayList, разделение по пробелам
+
+    while (splitMathExample.stream().count() > 1) {
+      if (splitMathExample.contains(POWER)) {
+        splitMathExample = performOperation(splitMathExample, POWER);
+      }
+      else if (splitMathExample.contains(MULTIPLICATION) && !splitMathExample.contains(DIVIDE)) {
+        splitMathExample = performOperation(splitMathExample, MULTIPLICATION);
+      }
+      else if (splitMathExample.contains(DIVIDE) && !splitMathExample.contains(MULTIPLICATION)) {
+        splitMathExample = performOperation(splitMathExample, DIVIDE);
+      }
+      else if (splitMathExample.contains(MULTIPLICATION) && splitMathExample.contains(DIVIDE)) {
+        if (splitMathExample.indexOf(MULTIPLICATION) < splitMathExample.indexOf(DIVIDE)) {
+          splitMathExample = performOperation(splitMathExample, MULTIPLICATION);
+        }
+        else if (splitMathExample.indexOf(DIVIDE) < splitMathExample.indexOf(MULTIPLICATION)) {
+          splitMathExample = performOperation(splitMathExample, DIVIDE);
+        }
+      }
+      else if (splitMathExample.contains(PLUS) && !splitMathExample.contains(MINUS)) {
+        splitMathExample = performOperation(splitMathExample, PLUS);
+      }
+      else if (splitMathExample.contains(MINUS) && !splitMathExample.contains(PLUS)) {
+        splitMathExample = performOperation(splitMathExample, MINUS);
+      }
+      else if (splitMathExample.contains(PLUS) && splitMathExample.contains(MINUS)) {
+        if (splitMathExample.indexOf(PLUS) < splitMathExample.indexOf(MINUS)) {
+          splitMathExample = performOperation(splitMathExample, PLUS);
+        }
+        else if (splitMathExample.indexOf(MINUS) < splitMathExample.indexOf(PLUS)) {
+          splitMathExample = performOperation(splitMathExample, MINUS);
+        }
+      }
+    }
+
+    String mathExampleResult = mathExample + " = " + splitMathExample.get(0);
+
+    FileManager fileManager = new FileManager();
+    fileManager.writeToFile("history.txt", mathExampleResult);
+
+    return splitMathExample.get(0);
+  }
+
+  /**
    * Выполнить операцию
+   *
    * @param mathExample пример, тип ArrayList
    * @param mathOperation математическая операция
    * @return
    */
-  public ArrayList<String> performOperation(ArrayList<String> mathExample, String mathOperation) {
+  private ArrayList<String> performOperation(ArrayList<String> mathExample, String mathOperation) {
     int index = mathExample.indexOf(mathOperation);
 
     double leftNumber = Double.parseDouble(mathExample.get(index - 1));
@@ -129,7 +204,7 @@ public class Calculator {
         break;
       case DIVIDE:
         if (rightNumber == 0) {
-          throw new IllegalArgumentException("Деление на 0");
+          throw new IllegalArgumentException("Division by 0");
         }
         operationResult = leftNumber / rightNumber;
         modifyMathExample(mathExample, operationResult, index);
@@ -148,6 +223,7 @@ public class Calculator {
 
   /**
    * Изменить ArrayList в соответствии
+   *
    * @param mathExample математический пример, тип ArrayList
    * @param operationResult результат математической операции
    * @param index индекс математической операции
@@ -165,6 +241,7 @@ public class Calculator {
 class FileManager {
   /**
    * Записать данные в файл
+   *
    * @param fileName имя файла
    * @param content содержимое файла
    */
@@ -226,9 +303,8 @@ class FileManager {
         System.out.println(data);
       }
       reader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+    } catch (FileNotFoundException ex) {
+      System.out.println(ex.getMessage());
     }
   }
 }
